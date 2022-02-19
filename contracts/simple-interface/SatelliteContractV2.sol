68df53f09b279e438b527c6af646a94528f4cd88
Boom <boom@Shubhams-MacBook-Pro.local>
2022-02-19 20:51:20 +0100

    restructured code into topic folders

--- contracts/simple-interface/SatelliteContractV2.sol
+++ contracts/simple-interface/SatelliteContractV2.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "./../interfaces/ILogic.sol";
+import "./../../interfaces/simple-interface/ILogic.sol";
 
 contract SatelliteContractV1 is ILogic {
 
