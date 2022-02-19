1edee2d61f9ee3944a055e3076be44bb4e2dfa18
Boom <boom@Shubhams-MacBook-Pro.local>
2022-02-19 20:44:09 +0100

    added data accessors of sattelite contract in main contract

--- contracts/MainContract.sol
+++ contracts/MainContract.sol
@@ -9,8 +9,25 @@ contract MainContract {
     constructor() {
         owner = msg.sender;
     }
+
     function upgradeTo(address newImplementation) public {
         require(msg.sender == owner, "Only Owner");
         satteliteContract = ILogic(newImplementation);
+    }
+    
+    function getFirstName() external returns(bytes32){
+        return satteliteContract.getFirstName();
+    } 
+
+    function getLastName() external returns(bytes32){
+        return satteliteContract.getLastName();
+    }
+
+    function setFirstName(bytes32 _firstName) external {
+        satteliteContract.setFirstName(_firstName);
+    }
+
+    function setLastName(bytes32 _lastName)  external{
+        satteliteContract.setLastName(_lastName);
     }
 }
