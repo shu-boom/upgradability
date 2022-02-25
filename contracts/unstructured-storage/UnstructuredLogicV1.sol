7b24e5b4d8a0fd737581775424ed9196c3a6d5e0
Boom <boom@Shubhams-MacBook-Pro.local>
2022-02-25 18:24:24 +0100

    cleaned of unnecessary comment

--- contracts/unstructured-storage/UnstructuredLogicV1.sol
+++ contracts/unstructured-storage/UnstructuredLogicV1.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.0;
 
 contract UnstructuredLogicV1 {
     uint256 val;
-    // Tip public function may be called internally; therefore, solidity copies the arguments for a public function to memory. Whereas, external functions are cheap 
-    // because they rely on calldata instead of memory 
+
     function getVal() external view returns(uint256) {
         return val;
     }
